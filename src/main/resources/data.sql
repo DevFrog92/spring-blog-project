e4cbe02ab91a30d2d9fd7d32c6627c2b28550059
DevFrog92 <yongsu1481@gmail.com>
2023-06-22 20:16:01 +0900

    add: data.sql for cold start problem

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into article (title, content) values  ('title 1', 'content1');
+insert into article (title, content) values  ('title 2', 'content2');
+insert into article (title, content) values  ('title 3', 'content3');
