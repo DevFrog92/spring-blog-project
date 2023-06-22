55a6a750e81801aac5176001dd0c534a89c26120
DevFrog92 <yongsu1481@gmail.com>
2023-06-22 14:54:14 +0900

    implement: BlogApiController and BlogApiControllerTest

---
+++ src/main/java/com/blog/controller/BlogApiController.java
@@ -0,0 +1,29 @@
+package com.blog.controller;
+
+import com.blog.domain.Article;
+import com.blog.dto.AddArticleRequest;
+import com.blog.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/articles")
+@RequiredArgsConstructor
+public class BlogApiController {
+    private final BlogService blogService;
+
+    @PostMapping
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
+        Article savedArticle = blogService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(savedArticle);
+    }
+
+
+
+}
