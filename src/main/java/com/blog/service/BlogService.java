1de98a02dcd1ed923704f1dc6b3e4c6f9b63d4ab
DevFrog92 <yongsu1481@gmail.com>
2023-06-22 14:53:52 +0900

    implement: Article DTO and BlogService

---
+++ src/main/java/com/blog/service/BlogService.java
@@ -0,0 +1,17 @@
+package com.blog.service;
+
+import com.blog.domain.Article;
+import com.blog.dto.AddArticleRequest;
+import com.blog.repository.BlogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class BlogService {
+    private final BlogRepository blogRepository;
+
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity());
+    }
+}
