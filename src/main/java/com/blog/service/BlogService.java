506a89cd8bdc4e503bfdf40e71de78947c5b18c9
DevFrog92 <yongsu1481@gmail.com>
2023-06-22 20:15:01 +0900

    update: BlogService for RUD

--- src/main/java/com/blog/service/BlogService.java
+++ src/main/java/com/blog/service/BlogService.java
@@ -2,9 +2,13 @@ package com.blog.service;
 
 import com.blog.domain.Article;
 import com.blog.dto.AddArticleRequest;
+import com.blog.dto.UpdateArticleRequest;
 import com.blog.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -13,5 +17,29 @@ public class BlogService {
 
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    public Article findById(long id){
+        return blogRepository.findById(id)
+                .orElseThrow(() ->
+                        new IllegalArgumentException("not found" + id));
+    }
+
+    public void delete(long id){
+        blogRepository.deleteById(id);
+    }
+
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request){
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() ->
+                        new IllegalArgumentException("not found "+ id));
+
+        article.update(request.getTitle(), request.getContent());
+        return article;
     }
 }
